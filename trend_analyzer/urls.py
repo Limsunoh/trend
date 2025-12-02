c2463a0f85477ae783f5a14b1805b696e5cea5ab
kwang1215 <xman1215@naver.com>
2025-12-02 16:27:11 +0900

    commit

--- trend_analyzer/urls.py
+++ trend_analyzer/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('api/dashboard/', include('dashboard.urls')),
     path('api/analyzer/', include('analyzer.urls')),
     path('api/collector/', include('data_collector.urls')),
-    path('api/user_qa/', include('qa.urls')),
+    path('api/user_qa/', include('user_qa.urls')),
 ]
 
 if settings.DEBUG:
